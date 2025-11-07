148c441d8289c9abc201fd736e877cdd2bba55b2
rssXX <31318078+rssXX@users.noreply.github.com>
2025-11-07 15:21:10 +0500

    task6

---
+++ src/lr1/task6.java
@@ -0,0 +1,13 @@
+package lr1;
+
+import java.time.Year;
+
+public class task6 {
+    public static void main(String[] args) {
+        String name = InputHelper.getInputString("Введите своё имя: ");
+        int birthdayYear = InputHelper.getInputInt("Введите год рождения: ");
+        int year = Year.now().getValue();
+
+        System.out.printf("%s возраст: %d", name, year - birthdayYear);
+    }
+}
