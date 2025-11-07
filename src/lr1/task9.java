f120482a200ff4ae9711302eafaf8e788671b1ee
rssXX <31318078+rssXX@users.noreply.github.com>
2025-11-07 15:35:53 +0500

    task9

---
+++ src/lr1/task9.java
@@ -0,0 +1,13 @@
+package lr1;
+
+public class task9 {
+    public static void main(String[] args) {
+        int num = InputHelper.getInputInt("Введите число: ");
+
+        int prev = num - 1;
+        int next = num + 1;
+        int sqrt = (int) Math.pow(prev + num + next, 2);
+
+        System.out.println("Последовательность: " + prev + ' ' + num + ' ' + next + ' ' + sqrt);
+    }
+}
