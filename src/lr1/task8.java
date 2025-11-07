c73728c8fd7a959896e4b56747b38a5d5174204c
rssXX <31318078+rssXX@users.noreply.github.com>
2025-11-07 15:27:22 +0500

    task8

---
+++ src/lr1/task8.java
@@ -0,0 +1,10 @@
+package lr1;
+
+public class task8 {
+    public static void main(String[] args) {
+        int a = InputHelper.getInputInt("Введите первое число: ");
+        int b = InputHelper.getInputInt("Введите второе число: ");
+
+        System.out.printf("Сумма чисел: %d", a + b);
+    }
+}
