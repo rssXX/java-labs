b6ded68edefa8251e86c6113b3dcedc9770afc53
rssXX <31318078+rssXX@users.noreply.github.com>
2025-11-07 16:05:24 +0500

    примеры из лаб.работы

---
+++ src/lr1/Example3.java
@@ -0,0 +1,24 @@
+package lr1;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int s, d;
+
+        s = 10;
+        d = 20;
+
+        if (s < d){
+            System.out.println("S < D");
+        }
+
+        s = s * 20;
+        if (s == d) {
+            System.out.println("S = D");
+        }
+
+        s = s * d;
+        if (s > d){
+            System.out.println("S > D");
+        }
+    }
+}
