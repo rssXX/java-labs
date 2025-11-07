8897a4981ea10200c90b102684288f3271615552
rssXX <31318078+rssXX@users.noreply.github.com>
2025-11-07 14:27:04 +0500

    task 1 - вынос функции

--- src/lr1/Example1.java
+++ src/lr1/Example1.java
@@ -1,20 +1,24 @@
+package lr1;
+
 import java.util.Scanner;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
-void main() throws UnsupportedEncodingException {
-    System.setOut(new PrintStream(System.out, true, "Cp866"));
-    Scanner in = new Scanner(System.in, "Cp866");
+public class Example1 {
+    public static String getInput(Scanner scanner, String quest) {
+        System.out.print(quest);
+        return scanner.nextLine();
+    }
 
-    System.out.print("Введите свою фамилию: ");
-    String surname = in.nextLine();
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.setOut(new PrintStream(System.out, true, "Cp866"));
+        Scanner in = new Scanner(System.in, "Cp866");
 
-    System.out.print("Введите свою имя: ");
-    String name = in.nextLine();
+        String surname = getInput(in, "Введите свою фамилию: ");
+        String name = getInput(in, "Введите своё имя: ");
+        String patronymic = getInput(in, "Введите своё отчество: ");
 
-    System.out.print("Введите свою отчество: ");
-    String patronymic = in.nextLine();
-
-    System.out.printf("Hello %s %s %s", surname, name, patronymic);
-    in.close();
+        System.out.printf("Hello %s %s %s", surname, name, patronymic);
+        in.close();
+    }
 }
